2084ac3a8fc91346096c1919bd9ed85819fe5324
dangsonghuong010405@gmail.com <dangsonghuong010405@gmail.com>
2026-01-31 00:26:12 +0700

    feat(store): configure Redux Toolkit for persistent auth state

---
+++ frontend/src/store/slices/auth/authSlice.ts
@@ -0,0 +1,45 @@
+import type { AuthState, LoginResponse } from "@/types/auth";
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+
+const initialState: AuthState = {
+  email: localStorage.getItem("email") || "",
+  firstName: localStorage.getItem("firstName") || "",
+  lastName: localStorage.getItem("lastName") || "",
+  role: localStorage.getItem("role") || "",
+  permissions: JSON.parse(localStorage.getItem("permissions") || "[]"),
+  isAuthenticated: !!localStorage.getItem("token"),
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setLogin: (state, action: PayloadAction<LoginResponse>) => {
+      const { permissions, email, firstName, lastName, role } = action.payload;
+      state.email = email;
+      state.permissions = permissions;
+      state.firstName = firstName;
+      state.lastName = lastName;
+      state.role = role;
+      state.isAuthenticated = true;
+
+      localStorage.setItem("email", email);
+      localStorage.setItem("firstName", firstName);
+      localStorage.setItem("lastName", lastName);
+      localStorage.setItem("role", role);
+      localStorage.setItem("permissions", JSON.stringify(permissions));
+    },
+    setLogout: (state) => {
+      state.email = "";
+      state.firstName = "";
+      state.lastName = "";
+      state.role = "";
+      state.permissions = [];
+      state.isAuthenticated = false;
+      localStorage.clear();
+    },
+  },
+});
+
+export const { setLogin, setLogout } = authSlice.actions;
+export default authSlice.reducer;
