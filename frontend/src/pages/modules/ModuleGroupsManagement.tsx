869969d625de5436334660e8d4a2d0d3e77c3504
pnhoanganh <pnhoanganhh@gmail.com>
2026-01-30 15:20:48 +0700

    feat(layout): add DynamicBreadcrumbs component

--- frontend/src/pages/modules/ModuleGroupsManagement.tsx
+++ frontend/src/pages/modules/ModuleGroupsManagement.tsx
@@ -1,12 +1,15 @@
 import {MainLayout} from "@/components/MainLayout";
 import {Card} from "@/components/ui/card";
 import ModuleGroupsTable from "@/pages/modules/module_groups/table.tsx";
+import DynamicBreadcrumbs from "@/components/DynamicBreadcrumbs.tsx";
 
 
-export default function ModulesManagement() {
+export default function ModuleGroupsManagement() {
     return (
         <MainLayout>
-            <div className="flex flex-col h-full gap-6 mx-2">
+
+            <div className="flex flex-col h-full gap-6">
+                <DynamicBreadcrumbs pathTitles={{moduleGroups: "Module Group Management"}}/>
                 {/* Card chiếm toàn bộ phần còn lại */}
                 <Card className="flex-1 flex flex-col px-6 py-8">
                     <ModuleGroupsTable/>
