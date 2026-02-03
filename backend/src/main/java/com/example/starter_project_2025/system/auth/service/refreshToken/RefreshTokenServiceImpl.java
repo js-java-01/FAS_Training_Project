21c4c31e1512e32191cf42677d53ff2a4a7ac59c
lequanghieu2504 <lequanghieu2504@gmail.com>
2026-02-03 12:29:53 +0700

    chore (util): refactor place to hold jwtutil

--- backend/src/main/java/com/example/starter_project_2025/system/auth/service/refreshToken/RefreshTokenServiceImpl.java
+++ backend/src/main/java/com/example/starter_project_2025/system/auth/service/refreshToken/RefreshTokenServiceImpl.java
@@ -4,7 +4,7 @@ import com.example.starter_project_2025.security.UserDetailsImpl;
 import com.example.starter_project_2025.system.auth.entity.RefreshToken;
 import com.example.starter_project_2025.system.auth.repository.RefreshTokenRepository;
 import com.example.starter_project_2025.system.user.entity.User;
-import com.example.starter_project_2025.security.JwtUtil;
+import com.example.starter_project_2025.util.JwtUtil;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
--- backend/src/main/java/com/example/starter_project_2025/util/JwtUtil.java
+++ backend/src/main/java/com/example/starter_project_2025/util/JwtUtil.java
@@ -1,5 +1,6 @@
-package com.example.starter_project_2025.security;
+package com.example.starter_project_2025.util;
 
+import com.example.starter_project_2025.security.UserDetailsImpl;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
