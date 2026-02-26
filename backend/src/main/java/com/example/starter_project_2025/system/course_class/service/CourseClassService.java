e94fcca7dd7197bbee0d280813f1b2a7df20e7e7
lequanghieu2504 <lequanghieu2504@gmail.com>
2026-02-26 15:48:29 +0700

    feat(course class): add api get all course class

---
+++ backend/src/main/java/com/example/starter_project_2025/system/course_class/service/CourseClassService.java
@@ -0,0 +1,10 @@
+package com.example.starter_project_2025.system.course_class.service;
+
+import com.example.starter_project_2025.system.course_class.dto.CourseClassResponse;
+
+import java.util.List;
+
+public interface CourseClassService {
+
+    List<CourseClassResponse> getAll();
+}
