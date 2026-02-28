b60fe29b590223b818bfc3e6f26ec3d84d2f43c3
lequanghieu2504 <lequanghieu2504@gmail.com>
2026-02-28 22:45:58 +0700

    feat (course-class): add api find course-class by classid and add data init

--- backend/src/main/java/com/example/starter_project_2025/system/course_class/repository/CourseClassRepository.java
+++ backend/src/main/java/com/example/starter_project_2025/system/course_class/repository/CourseClassRepository.java
@@ -4,8 +4,11 @@ import com.example.starter_project_2025.system.course_class.entity.CourseClass;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface CourseClassRepository extends JpaRepository<CourseClass, UUID> {
+
+    List<CourseClass> findByClassInfo_Id(UUID classId);
 }
--- backend/src/main/java/com/example/starter_project_2025/system/course_class/service/CourseClassService.java
+++ backend/src/main/java/com/example/starter_project_2025/system/course_class/service/CourseClassService.java
@@ -3,8 +3,11 @@ package com.example.starter_project_2025.system.course_class.service;
 import com.example.starter_project_2025.system.course_class.dto.CourseClassResponse;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface CourseClassService {
 
     List<CourseClassResponse> getAll();
+
+    List<CourseClassResponse> getByClassId(UUID classId);
 }
