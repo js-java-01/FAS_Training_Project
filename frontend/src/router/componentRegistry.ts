1f9fdddc313f69b12cdcafe93c7f6b4bc87d62c2
pnhoanganh <pnhoanganhh@gmail.com>
2026-03-02 13:30:12 +0700

    fix merge

--- frontend/src/router/componentRegistry.ts
+++ frontend/src/router/componentRegistry.ts
@@ -1,10 +1,9 @@
 import type { ComponentType } from "react";
-import { UserManagement } from "@/pages/UserManagement";
+import { UserManagement } from "@/pages/user/UserManagement";
 import { Dashboard } from "@/pages/Dashboard";
 import ModulesManagement from "@/pages/modules/module/ModulesManagement.tsx";
 import ModuleGroupsManagement from "@/pages/modules/module_groups/ModuleGroupsManagement.tsx";
 import NotFoundPage from "@/pages/NotFoundPage.tsx";
-import { RoleManagement } from "@/pages/RoleManagement.tsx";
 import { OwnClassPage } from "@/pages/training-classes/trainee/OwnClassPage";
 import TrainerClassesManagement from "@/pages/training-classes/trainer/TrainerClassesManagament";
 import LocationsManagement from "@/pages/locations/LocationsManagement";
@@ -12,6 +11,7 @@ import PermissionsManagement from "@/pages/permissions/PermissionsManagement";
 import TopicManagement from "@/pages/topic/TopicManagement";
 import ProgramManagement from "@/pages/programs/ProgramManagement";
 import ClassesComponent from "@/pages/classes/ClassesManagement";
+import { RoleManagement } from "@/pages/role/RoleManagement";
 
 
 export const componentRegistry: Record<string, ComponentType> = {
