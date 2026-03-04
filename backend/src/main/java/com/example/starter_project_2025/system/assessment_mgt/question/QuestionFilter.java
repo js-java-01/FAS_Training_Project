55de85bc3591d08c9dea0da13160b8bad1fc1add
khoahd14805 <huynhdangkhoaj7@gmail.com>
2026-03-04 19:27:49 +0700

    feat(Question) filter

--- backend/src/main/java/com/example/starter_project_2025/system/assessment_mgt/question/QuestionFilter.java
+++ backend/src/main/java/com/example/starter_project_2025/system/assessment_mgt/question/QuestionFilter.java
@@ -1,4 +1,30 @@
 package com.example.starter_project_2025.system.assessment_mgt.question;
 
-public class QuestionFilter {
-}
+import com.example.starter_project_2025.base.crud.dto.FilterOperator;
+import com.example.starter_project_2025.base.spec.FilterField;
+import lombok.Builder;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+@Builder
+public record QuestionFilter(
+
+        @FilterField(entityField = "questionType")
+        String questionType,
+
+        @FilterField(entityField = "isActive")
+        Boolean isActive,
+
+        @FilterField(entityField = "category.id")
+        UUID categoryId,
+
+        @FilterField(entityField = "tags.id", operator = FilterOperator.IN)
+        Set<Long> tagIds,
+
+        @FilterField(entityField = "createdAt", operator = FilterOperator.BETWEEN)
+        List<LocalDate> createdRange
+
+) {}
