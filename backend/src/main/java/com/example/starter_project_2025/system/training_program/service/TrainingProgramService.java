dc246bec0b93e25d4e952553e1c395405196bf69
karmalinh <142292959+karmalinh@users.noreply.github.com>
2026-03-05 10:17:40 +0700

    fix (classes, training prg): fix filter and search

--- backend/src/main/java/com/example/starter_project_2025/system/training_program/service/TrainingProgramService.java
+++ backend/src/main/java/com/example/starter_project_2025/system/training_program/service/TrainingProgramService.java
@@ -10,12 +10,14 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.List;
 import java.util.UUID;
 
 public interface TrainingProgramService {
 
     Page<TrainingProgramResponse> searchTrainingPrograms(
             String keyword,
+            List<String> versions,
             Pageable pageable
     );
     TrainingProgramResponse create(CreateTrainingProgramRequest request);
